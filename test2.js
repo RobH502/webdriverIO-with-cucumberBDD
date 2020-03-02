5a36d6026cc0aa4b359726d53461952fa2a8602f
RobH502 <robert.hayes@auticon.us>
2020-03-02 15:34:52 -0800

    New Js file

--- test2.js
+++ test2.js
@@ -1,1 +1,20 @@
-var i = 1;
+var i = 0;
+
+var m = [{
+    name: "Robert",
+    age: 28,
+    gender: "Male",
+    pets: ["Dog", "Cat"]
+},
+{
+    name: "Taki",
+    age: 18,
+    gender: "Male"
+}];
+
+function addCount(value) {
+    i += value;
+}
+
+m[0].pets.pop();
+console.log(m[0].pets);
