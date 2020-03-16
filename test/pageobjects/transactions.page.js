20478097e253105fe9b6d987f7b4b5103ea57d6e
RobH502 <robert.hayes@auticon.us>
2020-03-16 15:33:25 -0700

    Edited transactions.js

--- test/pageobjects/transactions.page.js
+++ test/pageobjects/transactions.page.js
@@ -10,11 +10,30 @@ class Transactions extends Page {
     //Shopping cart link
     get cartLink() { $('a[@class="cart-label"]'); }
 
+    //Add to cart
+    get addToCart() { $('//*[@data-productid="14"]//child::*//[@class="details"]//child:://*[@class="add-info"]//child:://*[@class="buttons"]//input[@value="Add to cart"]'); }
+
+    //Shopping cart
+    get countryDropdown() { return $('select[@id="CountryId"]'); }
+    get stateProvinceDropdown() { return $('select[@id="StateProvinceId"]'); }
+    get zipPostalCode() { reutnr $('input[@id="ZipPostalCode"]'); }
+    get termsOfServiceCheckbox() { return $('input[@id="termsofservice"]'); }
+    get checkoutButton() { return $('button[@id="checkout"]'); }
+
 
     //************************************************************************
 
     open() {
         super.open('video-games');
+    }
+
+    toCart() {
+        this.cartLink.click();
+    }
+
+    checkout() {
+        this.termsOfServiceCheckbox.click();
+        this.checkoutButton.click();
     }
 }
 
