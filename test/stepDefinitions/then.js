217c3750f70030b1172fb2d80f7d946ba4c3f00b
RobH502 <robert.hayes@auticon.us>
2020-03-18 10:04:11 -0700

    Updated when.js

--- test/stepDefinitions/then.js
+++ test/stepDefinitions/then.js
@@ -4,6 +4,7 @@ import registerPage from '../pageobjects/registration.page';
 import myAccountPage from '../pageobjects/myaccount.page';
 import videoGamesPage from '../pageobjects/videogames.page';
 import footerSection from '../pageobjects/footer.page';
+import transactionsPage from '../pageobjects/transactions.page';
 
 
 //Login
@@ -35,8 +36,13 @@ Then(/^I should see the "([^"]*)" header$/, function (header) {
 });
 
 Then(/^I should see the new address block and its name headers "([^"]*)" "([^"]*)"$/, function(fName, lName) {
-  myAccountPage.addressVerifyPart1(fName, lName);
+  myAccountPage.addressVerifyPresent(fName, lName);
 });
+
+Then(/^the address is no longer displayed on the page "(\D+)" "(\D+)"$/, function(fName, lName) {
+  myAccountPage.addressVerifyDeleted(fName, lName);
+});
+
 
 //Video Games
 Then('I see the Video Game header on the page', function() {
@@ -57,4 +63,18 @@ Then(/^the page with the url "(\D+)" should load in a separate tab$/, function(u
   footerSection.loadedFollowUsPage(url);
   browser.closeWindow();
   browser.switchWindow('https://auticontraining.azurewebsites.net/');
+});
+
+
+//Transactions
+Then('I see a confirmation message at the top of the screen', function() {
+  transactionsPage.addCartConfirmation();
+});
+
+Then('I see a popup message saying Out of stock at the top of the screen', function() {
+  transactionsPage.soldOut();
+});
+
+Then('I should see the order displayed with status pending', function() {
+  transactionsPage.transactionVerification();
 });
